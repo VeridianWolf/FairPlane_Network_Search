153708375f9a6c35cb343919947e421f6c9bd16a
jackboxlegal <jack@boxlegal.co.uk>
2017-11-17 10:58:05 +0000

    documented php as much as i could

--- scripts/doSearch.php
+++ scripts/doSearch.php
@@ -1,8 +1,40 @@
 <?php
-# order_id, email and surname are all empty variables
+#include these files from codelibrary
+require_once($_SERVER["DOCUMENT_ROOT"]	.	"/codelibrary/includes/php/autoload.php");
+require_once($_SERVER["DOCUMENT_ROOT"]	.	"/codelibrary/includes/php/common.php");
+require_once("/config.inc.php");
+
+
+# mysql = new connection to Mysql_Library()
+$gMysql				=	new Mysql_Library();
+
+# default return code
+$returnCode			=	"error";
+
+# this is the return array with all values
+$returnArray		=	array();
+
+# initialize this variable, as it may be added to later.
+# message variable currently empty
+$message			=	"";
+
+
+
+# $order_id,
+# $email
+# $surname
+# are all empty variables
 $order_id = "";
 $email = "";
 $surname = "";
+
+
+# get the variables from $_POST
+# gets the order_id, email and suranme that are entered into the input fields
+$order_id			= GetVariableString('search_order_id',$_POST,"");
+$email			    = GetVariableString('search_email',$_POST,"");
+$surname			= GetVariableString('search_surname',$_POST,"");
+
 
 # searches SQL table fp_flight_master_db_flight_info WHERE
 $sqlString	=	" select * from fp_flight_master_db_flight_info WHERE ";
@@ -25,25 +57,33 @@ if (!empty($surname))
     $sqlString		.=	" AND surname like '%" . $surname ."%' ";
 }
 
-#empty array called data
-$data = array();
 
-# variable $num_items = count the items within $returnArray
-$num_items = count($returnArray);
+# this variable is a two dimensional array
+# data_2d_array is equal to gMysql calls function selectToArray
+$data_2d_array	=	$gMysql->selectToArray($sqlString,__FILE__,__LINE__);
 
-#start table outside of foreach loop
+
+# variable $num_items = count the items within $data_2d_array (whole table)
+$num_items = count($data_2d_array);
+
+#start table (outside of foreach loop)
 $html = '<table>';
 
 
-#start table headers outside of loop also as these won't change
-$html .= "<tr>";
-$html .= "<th>Order Id<th/>";
-$html .= "<th>Email Address<th/>";
-$html .= "<th>Surname<th/>";
-$html .= "</tr>";
+#start table headers
+#start table headers (outside of loop also as these won't change)
+$html .= "<tr>
+            <th>Order Id<th/>
+            <th>Email Address<th/>
+            <th>Surname<th/>
+          </tr>";
 
 
-foreach ($returnArray as $data)
+
+
+# this loops will go through the data_2d_array (table)
+# and store each arrays value as a variable ($data)
+foreach ($data_2d_array as $data)
 {
     # grabs the data from each column of the database id / email / surname
     $id = $data['id'];
@@ -51,15 +91,17 @@ foreach ($returnArray as $data)
     $surname = $data['surname'];
 
     # this part of the table will be filled with the info that is grabbed from the database using the code above
-    $html .= "<tr>";
-    $html .= "<td>$id<td/>";
-    $html .= "<td>$email<td/>";
-    $html .= "<td>$surname<td/>";
-    $html .= "</tr>";
+    $html .= "<tr>
+                <td>$id<td/>
+                <td>$email<td/>
+                <td>$surname<td/>
+             </tr>";
 }
 
-    #finish table - close the table html tag
-    $html .= '</table>';
+
+
+#finish table - close the table html tag
+$html .= '</table>';
 
 
 
