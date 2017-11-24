1fb9c901ffff6e9ec421a418adfe0ade2fcee913
jackboxlegal <jack@boxlegal.co.uk>
2017-11-24 17:02:44 +0000

    Working search... search by any field and shows table results on screen.
    documented code as well

--- scripts/doSearch.php
+++ scripts/doSearch.php
@@ -1,5 +1,5 @@
 <?php
-#include these files from codelibrary
+# include these files from codelibrary
 require_once($_SERVER["DOCUMENT_ROOT"]	.	"../codelibrary/includes/php/autoload.php");
 require_once($_SERVER["DOCUMENT_ROOT"]	.	"../codelibrary/includes/php/common.php");
 require_once ("../config.inc.php");
@@ -36,33 +36,42 @@ $surname			= GetVariableString('search_surname',$_POST,"");
 # searches SQL table fp_flight_master_db_flight_info WHERE
 $sqlString	=	" select * from fp_flight_master_db_flight_info WHERE ";
 
-#if $order_id is not empty then add $order_id to $sqlString
+# if $order_id is not empty then add $order_id to $sqlString
 if (!empty($order_id))
 {
     $sqlString		.=	" order_id like '%" . $order_id ."%' ";
 }
-#if $email is not empty then add $email to $sqlString
+
+# if $email is not empty then add $email to $sqlString
 if (!empty($email))
 {
+    # if order_id is not empty then add AND to $sqlString
     if (!empty($order_id))
     {
         $sqlString .=" AND ";
     }
+
+    # sqlString concatenated email like $email (using wildcard charcaters '%"
     $sqlString		.=	"  email like '%" . $email ."%' ";
 }
-#if $surname is not empty then add $surname to $sqlString
+
+# if $surname is not empty then add $surname to $sqlString
 if (!empty($surname))
 {
+    # if order_id or $email are are not empty then add AND to $sqlString
     if ((!empty($order_id)) || (!empty($email)))
     {
         $sqlString .=" AND ";
     }
+
+    # sqlString concatenated surname like $surname (using wildcard charcaters '%"
     $sqlString		.=	"  surname like '%" . $surname ."%' ";
 }
 
+# if $order_id and $email and $surname are empty then add sql syntax 1 = 1 LIMIT 100 to $sqlString
 if (empty($order_id) && empty($email) && empty($surname))
 {
-    $sqlString .= " 1 = 1 ";
+    $sqlString .= " 1 = 1 LIMIT 100 ";
 }
 
 # this variable is a two dimensional array
@@ -73,46 +82,55 @@ $data_2d_array	=	$gMysql->selectToArray($sqlString,__FILE__,__LINE__);
 # variable $num_items = counts the items within $data_2d_array (whole table)
 $num_items = count($data_2d_array);
 
-#start table (outside of foreach loop)
-$html = '<table>';
+# start table (outside of foreach loop)
+$html = '<table id="myTable" class="display" cellspacing="0" width="100%">';
 
 
-#start table headers
-#start table headers (outside of loop also as these won't change)
-$html .= "<tr>
-            <th>Order Id<th/>
-            <th>Email Address<th/>
-            <th>Surname<th/>
-          </tr>";
+# start table headers
+# start table headers (outside of loop also as these won't change)
+$html .= "    <thead> 
+                    <tr>
+                        <th>Order Id<th/>
+                        <th>Email Address<th/>
+                        <th>Surname<th/>
+                    </tr>
+                </thead>";
 
-
-
+# open the tbody tag
+$html .= "<tbody>";
 
 # this loop will go through the data_2d_array (table)
 # and store each arrays value as a variable ($data)
 foreach ($data_2d_array as $data)
 {
     # grabs the data from each column of the database id / email / surname
-    $id = $data['id'];
+    $order_id = $data['order_id'];
     $email = $data['email'];
     $surname = $data['surname'];
 
+
+
     # this part of the table will be filled with the info that is grabbed from the database using the code above
-    $html .= "<tr>
-                <td>$id<td/>
-                <td>$email<td/>
-                <td>$surname<td/>
-             </tr>";
+    $html .= "   <tr>
+                    <td>$order_id<td/>
+                    <td>$email<td/>
+                    <td>$surname<td/>
+                 </tr>";
+
+
+
 }
+# close the tbody tag
+$html .= "</tbody>";
 
-
-#finish table - close the table html tag
+# finish table - close the table html tag
 $html .= '</table>';
 
 
 # returns the data in this array to the javascript ajax
 $returnArray    =   array	(
 
+    # where result is replace with variable $html
     "result" => $html,
 
 );
