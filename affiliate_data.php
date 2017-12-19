354c4a025057444b5ddb90cab6f1c73d0bc07649
jackboxlegal <jack@boxlegal.co.uk>
2017-12-19 10:44:03 +0000

    Row is clickable goes to new page... Need to make payout status changeable

--- affiliate_data.php
+++ affiliate_data.php
@@ -11,12 +11,27 @@ include ("app/includes/php/template.php");
 # mysql = new connection to Mysql_Library()
 $gMysql				=	new Mysql_Library();
 
-# this is the return array with all values
-$returnArray		=	array();
-
-$data	=	$gMysql->queryRow("SELECT * FROM 'fp_flight_master_db_flight_info'  ",__FILE__,__LINE__);
+$proclaim_id			= GetVariableString('proclaim_id',$_GET,"");
 
 
+$data	=	$gMysql->queryRow("SELECT * FROM fp_flight_master_db_flight_info WHERE proclaim_id='$proclaim_id'  ",__FILE__,__LINE__);
+
+if(empty($data)){
+    gotoURL("/?pUpdate=IDNotFound");
+    exit;
+}
+else
+{
+    $case_input_data    =   $data['case_input_date'];
+    $case_key           =   $data['case_key'];
+    $email              =   $data['email'];
+    $forename           =   $data['forename'];
+    $surname            =   $data['surname'];
+    $status             =   $data['status'];
+    $case_status        =   $data['case_status'];
+    $payout_status      =   $data['payout_status'];
+    $affiliate          =   $data['affiliate'];
+}
 
 
 # variable template = function getTemplate points to file index.html
@@ -29,9 +44,19 @@ $header = getTemplate("app/templates/header.html");
 $footer = getTemplate("app/templates/footer.html");
 
 
+
 # variable template = string replace {{header}} with the variable $header
 # variable template = string replace {{footer}} with the variable $footer
 $template = str_replace("{{header}}", $header, $template);
 $template = str_replace("{{footer}}", $footer, $template);
+$template = str_replace("{{case_input_data}}", $case_input_data, $template);
+$template = str_replace("{{case_key}}", $case_key, $template);
+$template = str_replace("{{email}}", $email, $template);
+$template = str_replace("{{forename}}", $forename, $template);
+$template = str_replace("{{surname}}", $surname, $template);
+$template = str_replace("{{status}}", $status, $template);
+$template = str_replace("{{case_status}}", $case_status, $template);
+$template = str_replace("{{payout_status}}", $payout_status, $template);
+$template = str_replace("{{affiliate}}", $affiliate, $template);
 
 echo $template;
--- config.inc.php
+++ config.inc.php
@@ -68,3 +68,348 @@ define	('MYSQL_CACHE_TIME_LONG',		60*60);
 define	('MYSQL_CACHE_TIME_HUGE',		24*60*60);
 define	('MYSQL_CACHE_TIME_INFINITE',	7*24*60*60);
 define	('MYSQL_CACHE_TIME_BEYOND',		60*24*60*60);
+
+
+
+
+
+
+$gPopupLookUp	=
+    array(
+
+
+        "IDNotFound" 		=> array("html" =>	"
+		$.alert({
+			
+				title: 'Thank You',
+				confirmButton: 'Ok',
+				content: 'Thank you for registering. An account activation link has been sent to your email address.',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'material',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+				escapeKey: true,
+				backgroundDismiss: true,
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'green',
+				typeAnimated: 'true',
+							
+
+    			});
+
+				"),
+        "passwordSent" 		=> array("html" =>	"
+		$.alert({
+			
+				title: 'Thank You',
+				confirmButton: 'Ok',
+				content: 'Your password change link has been sent. Please use it to create a new password for your account.',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'material',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+				escapeKey: true,
+				backgroundDismiss: true,
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'green',
+				typeAnimated: 'true',
+							
+
+    			});
+
+				"),
+
+        "passwordChanged" 		=> array("html" =>	"
+		$.alert({
+			
+				title: 'Thank You',
+				confirmButton: 'Ok',
+				content: 'Your password has been changed. Please use it to log in to your account.',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'material',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+				escapeKey: true,
+				backgroundDismiss: true,
+
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'green',
+				typeAnimated: 'true',
+	
+    			});
+
+				"),
+        "loginProblem" 		=> array("html" =>	"
+		$.alert({
+		
+			
+				title: 'Error',
+				confirmButton: 'Ok',
+				content: 'Your login details were not recognised.<br>Please try again.',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'material',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+    			escapeKey: true,
+    			
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'red',
+				typeAnimated: 'true',
+	    			
+    			
+    			});
+
+				"),
+        "loggedOut" 		=> array("html" =>	"
+		$.alert({
+		
+			
+				title: 'Thank You',
+				confirmButton: 'Ok',
+				content: 'You have now been logged out.',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'material',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+    			escapeKey: true,
+
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'green',
+				typeAnimated: 'true',
+	
+    			});
+
+				"),
+
+        "contactSent" 		=> array("html" =>	"
+		$.alert({
+		
+			
+				title: 'Thank You',
+				confirmButton: 'Ok',
+				content: 'Your message has been received.<br>We will be in touch shortly',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'material',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+    			escapeKey: true,
+    			
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'green',
+				typeAnimated: 'true',
+	    			
+    			
+    			});
+
+				"),
+
+        "activateOK" 		=> array("html" =>	"
+		$.alert({
+		
+			
+				title: 'Thank You',
+				confirmButton: 'Ok',
+				content: 'Your account has been activated.<br>You may now login',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'bootstrap',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+    			escapeKey: true,
+    			
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'green',
+				typeAnimated: 'true',
+	    			
+    			
+    			});
+
+				"),
+
+        "activateInvalid" 		=> array("html" =>	"
+		$.alert({
+		
+			
+				title: 'A Problem has occured',
+				confirmButton: 'Ok',
+				content: 'Your code has expired or is invalid.<br>Please request another.',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'bootstrap',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+    			escapeKey: true,
+    			
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'red',
+				typeAnimated: 'true',
+	    			
+    			
+    			});
+
+				"),
+
+
+
+        "linkExpired" 		=> array("html" =>	"
+		$.alert({
+		
+			
+				title: 'A Problem has occured',
+				confirmButton: 'Ok',
+				content: 'Your link has expired or is invalid.<br>Please request another.',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'bootstrap',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+    			escapeKey: true,
+    			
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'red',
+				typeAnimated: 'true',
+	    			
+    			});
+
+				"),
+
+
+
+
+        "activated" 		=> array("html" =>	"
+		$.alert({
+		
+			
+				title: 'Thank you',
+				confirmButton: 'Ok',
+				content: 'Your account has been activated already.<br>You may now login',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'bootstrap',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+    			escapeKey: true,
+    			
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'green',
+				typeAnimated: 'true',
+	    			
+    			
+    			});
+
+				"),
+
+
+
+
+        "claimError" 		=> array("html" =>	"
+		$.alert({
+		
+			
+				title: 'A Problem has occured',
+				confirmButton: 'Ok',
+				content: 'There was an error in the policy reference supplied.<br>Please select a policy to view',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+ 	 			theme: 'bootstrap',
+				confirmButtonClass: 'btn-success',
+				cancelButtonClass: 'btn-danger',
+				confirmButton: 'Ok',
+				cancelButton: 'Cancel',
+    			escapeKey: true,
+    			
+		
+				theme: 'material',
+				animation: 'scale',
+				type: 'red',
+				typeAnimated: 'true',
+	    			
+    			
+    			});
+
+				"),
+
+
+
+
+
+        "error" 		=> array("html" =>	"
+		$.alert({
+				title: 'Error',
+				confirmButton: 'Ok',
+				content: 'You seem to have arrived here in error',
+ 				closeIcon: true,
+    			closeIconClass: 'fa fa-close',
+    			animationSpeed: 200,
+    			});
+
+				"),
+
+
+
+
+
+    );
+
+
+
+
+
+
