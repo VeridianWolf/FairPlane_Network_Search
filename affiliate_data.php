b2c96325b67c829e36a1790e65f7012b3cd2b06d
jackboxlegal <jack@boxlegal.co.uk>
2018-01-02 10:39:10 +0000

    went through code as a refresher - updated some commented code.

--- affiliate_data.php
+++ affiliate_data.php
@@ -1,5 +1,6 @@
 <?php
-#include these files from codelibrary
+# layout - firstly we add the required/include files
+# include these files from codelibrary
 require_once($_SERVER["DOCUMENT_ROOT"]	.	"/codelibrary/includes/php/autoload.php");
 require_once($_SERVER["DOCUMENT_ROOT"]	.	"/codelibrary/includes/php/common.php");
 require_once("/config.inc.php");
@@ -7,19 +8,26 @@ require_once("/config.inc.php");
 
 include ("app/includes/php/template.php");
 
-
+# next we must open a connection to the sql database - Mysql_Library
 # mysql = new connection to Mysql_Library()
 $gMysql				=	new Mysql_Library();
 
+# here we use the function GetVariableString and this line of code GETS the proclaim id from the variable proclaim_id
 $proclaim_id			= GetVariableString('proclaim_id',$_GET,"");
 
-
+# now you can begin the query the database for what it is you want.
+# data is qual to the proclaim_id in the table fp_flight_master_db_flight_info
 $data	=	$gMysql->queryRow("SELECT * FROM fp_flight_master_db_flight_info WHERE proclaim_id='$proclaim_id'  ",__FILE__,__LINE__);
 
+
+# now if the data we are trying to GET is not there we must tell the code to do something about this.
+# for this we use the if else statement
 if(empty($data)){
+    # this line says that if variable $data is empty then you it should refresh the url to say /?pUpdate=IDNotFound and refresh the page
     gotoURL("/?pUpdate=IDNotFound");
     exit;
 }
+# else the variable the variable $case_input_data is equal to the data from the database under case_input_data etc ...
 else
 {
     $case_input_data    =   $data['case_input_date'];
@@ -42,7 +50,6 @@ $options_names  =   array(
     'Declined',                   #03
 );
 
-
 $options_values  =   array(
     '',                                                 # -- Select --
     'A',                                               # Approved
@@ -50,26 +57,31 @@ $options_values  =   array(
     'D',                                               # Declined
 );
 
+# this variable is used to build the drop down
 $select_string  =   '<select class="form-control" id="case_status" name="case_status" >';
 
+# here we use count to go through the dropdown options stored in the array list we made earlier
 $num    =   count($options_names);
 
-
+# and now we can begin our loop
+# for ($i=0; when i is less than num; add 1 ($i++) )
 for ($i=0;  $i < $num;  $i++)
 {
-
+    # variable $name is equal to $option_names[$i]
     $name = $options_names[$i];
-
+    # variable $value is equal to $options_values[$i]
     $value = $options_values[$i];
 
-
+    # if variable case_status is equal to the variable value
+    # then append to variable $select_string <option value='".$value."' selected=''>$name</option>
     if ($case_status == $value) {
         $select_string .= "<option value='".$value."' selected=''>$name</option>";
     }
 
-
+    # else
     else
     {
+        # append <option value='".$value."'>$name</option> to variable select_string
         $select_string .= "<option value='".$value."'>$name</option>";
     }
 
--- app/includes/php/server_processing.php
+++ app/includes/php/server_processing.php
@@ -1,51 +1,41 @@
 <?php
 
 /*
- * DataTables example server-side processing script.
+ * DataTables server-side processing script.
  *
- * Please note that this script is intentionally extremely simply to show how
- * server-side processing can be implemented, and probably shouldn't be used as
- * the basis for a large complex system. It is suitable for simple use cases as
- * for learning.
- *
- * See http://datatables.net/usage/server-side for full details on the server-
- * side processing requirements of DataTables.
- *
- * @license MIT - http://datatables.net/license_mit
  */
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Easy set variables
- */
-
-// DB table to use
+# database table to use
 $table = 'fp_flight_master_db_flight_info';
 
-// Table's primary key
+# table's primary key
 $primaryKey = 'id';
 
 
 # this excludes all claims where there is NO order_id or NO affiliate
 # no order_id really means there is no affiliate also, so it's a double safety measure
-
 $where = " order_id !='' and affiliate !='' ";
-// Array of database columns which should be read and sent back to DataTables.
-// The `db` parameter represents the column name in the database, while the `dt`
-// parameter represents the DataTables column identifier. In this case simple
-// indexes
 
 
+/*
+ * BELOW IS AN EXPLANATION OF HOW THE FOLLOWING $columns ARRAY LIST WORKS
+ *
+ */
+
+# Array of database columns which should be read and sent back to DataTables.
+# The `db` parameter represents the column name in the database, while the `dt`
+# parameter represents the DataTables column identifier. In this case simple indexes
 # $d = contains the variable (db) that has been extracted from the database
 # dt is the index for each item, starting at 0
 # $row[]  use this to pick an item from the row of variables
 # return - always return something if you are using 'formatter'
 
-
+#
 $columns = array(
 	array(
 		'db'        => 'case_input_date',
 		'dt'        => 0,
-		'formatter' => function( $d, $row ) {
+		'formatter' => function( $d, $row) {
 
 			if	($d == "0000-00-00 00:00:00")
 			{
@@ -88,7 +78,7 @@ $columns = array(
 
 		},
 	),
-
+    # array ( database column case_status, row index 6
 	array(
 		'db'        => 'case_status',
 		'dt'        => 6,
@@ -98,17 +88,28 @@ $columns = array(
 			# some claims can have no order_id. These claims are ones that are not made via the website
 			$order_id		=	$row[8];
 			# if there is no order_id, then we should bypass this bit
+            # if order_id is not empty
 			if (!empty($order_id))
 			{
+			    # D = declined
+                # A = Approved
+                # P = Pending
+
+			    # if $d is == to D
+                # return button with btn-danger class
 				if	($d == "D")
 				{
 					return "<button class='btn btn-danger'>Declined</button>";
 				}
-				else if	($d == "A")
+                # else if $d is == to A
+                # return button with btn-success class
+                else if	($d == "A")
 				{
 					return "<button class='btn btn-success'>Approved</button>";
 				}
-				else if	($d == "P")
+                # else if $d is == to P
+                # return button with btn-warning class
+                else if	($d == "P")
 				{
 					return "<button class='btn btn-warning'>Pending</button>";
 				}
@@ -119,11 +120,13 @@ $columns = array(
 		},
 	),
 
-
+    # array ( database column name affiliate,  row index 7 )
 	array( 'db' => 'affiliate',  'dt' => 7 ),
 
+    # array ( database column name order_id,  row index 8 )
     array( 'db' => 'order_id',  'dt' => 8 ),
 
+    # array ( database column name proclaim_id,  row index 9 )
     array( 'db' => 'proclaim_id',  'dt' => 9 ),
 
 
