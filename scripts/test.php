61b342d37a5152f164ec06b31db45b2d9e0dc3eb
jackboxlegal <jack@boxlegal.co.uk>
2018-01-16 16:38:14 +0000

    change file layout to correct format - all working

--- scripts/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-#include these files from codelibrary
-require_once($_SERVER["DOCUMENT_ROOT"]	.	"../codelibrary/includes/php/autoload.php");
-require_once($_SERVER["DOCUMENT_ROOT"]	.	"../codelibrary/includes/php/common.php");
-require_once ("../config.inc.php");
-
-
-echo "<h2>Search Results:</h2>";
-echo "{{table}}";
